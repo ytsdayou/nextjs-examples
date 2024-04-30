f963657900c233d60a4cf8bb081917ae4684bf9a
ytsdayou <ytsdayou@gmail.com>
2024-04-30 12:53:21 +0800

    redirect

--- use-iron-session/app/dashboard/layout.tsx
+++ use-iron-session/app/dashboard/layout.tsx
@@ -1,7 +1,15 @@
 import Menu from "@/components/menu";
 import Logout from "@/components/logout";
+import { getSession } from "@/actions/login";
+import { redirect } from "next/navigation";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await getSession();
+
+  if (!session.isLoggedIn) {
+    redirect('/login');
+  }
+
   return <main className="h-screen flex">
     <div className="bg-gray-700 w-72 text-slate-500">
       <div className="my-10 text-4xl text-center text-slate-300">Admin Panel</div>
